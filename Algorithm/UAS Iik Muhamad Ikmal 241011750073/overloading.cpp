a9240d5e69c7e4a2b9c828bc9c8a9d1442dcaa4a
IIK99 <muhamad.ikmal1099@gmail.com>
2025-06-28 20:35:37 +0700

    commit nih

--- Algorithm/UAS Iik Muhamad Ikmal 241011750073/overloading.cpp
+++ Algorithm/UAS Iik Muhamad Ikmal 241011750073/overloading.cpp
@@ -14,3 +14,6 @@ int main() {
     cout << "Hasil : " << prosess(4,5);
     return 0;
 }
+
+// ketika saya balik float di atas dan int di bawah hasilnya tetap sama 9
+// ku kira dari main function ternyata bukan juga, ketika main function diubah ke float jadi error 
